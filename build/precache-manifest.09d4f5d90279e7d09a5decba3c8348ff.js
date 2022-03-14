79368ca0a0d770668fc4ebb743bcceda6b976d73
drnareshchauhan <drnareshchauhan@gmail.com>
2022-03-14 13:49:37 +0530

    new oe

---
+++ build/precache-manifest.09d4f5d90279e7d09a5decba3c8348ff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad2c9dbc14a2b59ac1467b811c23c605",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58bd3ac16aff6aba94ea",
+    "url": "/static/css/2.46439c26.chunk.css"
+  },
+  {
+    "revision": "b1bcbfab733fb74c9be0",
+    "url": "/static/css/main.d1cee659.chunk.css"
+  },
+  {
+    "revision": "58bd3ac16aff6aba94ea",
+    "url": "/static/js/2.f9406310.chunk.js"
+  },
+  {
+    "revision": "b1bcbfab733fb74c9be0",
+    "url": "/static/js/main.8cc49bbc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4401e82d47a16230fd580bcbdb7c5d49",
+    "url": "/static/media/homepage.4401e82d.jpeg"
+  },
+  {
+    "revision": "29212f5ee6f1829c59a8b4735f2e513e",
+    "url": "/static/media/main.29212f5e.jpg"
+  }
+]);
---
+++ src/services/conf.js
@@ -0,0 +1,5 @@
+const base = 'https://examonlineapi.herokuapp.com';
+const LoginApi = '/api/v1/login';
+const FetchUserDetailsApi = '/api/v1/user/details';
+
+module.exports={ base, LoginApi, FetchUserDetailsApi }
